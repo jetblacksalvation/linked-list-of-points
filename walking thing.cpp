06d44c28625bc3a29f62e08abf2c357f906ebefb
jetblacksavior <coolanimeguy666@gmail.com>
2022-05-19 08:47:49 -0600

    version two

--- walking thing.cpp
+++ walking thing.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <vector>
 #include "sqaure.h"
-square thing;
+square thing(0, 0, 4, 40, true, sf::Color::Blue);
 struct points {
     float x, y;
     points* next;// looks at the next thing;
@@ -15,12 +15,12 @@ struct points {
     };
     points(int newx, int newy,points* newthing, sf::RenderWindow& nextwind) {
         x = newx;
-        y - newy;
+        y = newy;
         next = newthing;
         if (next != NULL) {
-            next->drawat(nextwind);
+            this->drawat(nextwind);
         }
-        else {
+        else {// this should NEVER be reached with how i have it set up!!!
             std::cout << "end of linked list, f off\n";//this is a linkd of points- below main i will define the algorithm
             // and eventually impliment it into the code
         }
@@ -29,11 +29,33 @@ struct points {
 
     };
     void drawat(sf::RenderWindow& wind) {
-        thing.x = this->x;
-        thing.y = this->y;
-        thing.draw(wind);
-        wind.clear({0,0,0});
+        if (next != NULL) {
 
+            while (this->x != next->x and this->y != next->y) {
+                thing.x = this->x;
+                thing.y = this->y;
+                thing.draw(wind);
+                if (this->x < next->x) {
+                    this->x++;
+                }
+                else if (this->x > next->x) {
+                    this->x--;
+                }
+                if (this->y > next->y) {
+                    this->y--;
+                }
+                else if (this->y < next->y) {
+                    this->y++;
+                }
+
+                if (this->x == next->x and this->y == next->y) { std::cout << "exit condition reached\n"; return; }
+            }
+
+        }
+        if (next == NULL) {
+            std::cout << "is null\n";
+        }
+        
     };
 
 };
@@ -42,7 +64,7 @@ struct points {
 int main()
 {
     for (int o = 1; o <= 15; o++) {
-        new points(5*o,5*5,);
+  
 
 
     }
@@ -50,12 +72,16 @@ int main()
 
     sf::RenderWindow window(sf::VideoMode(1000, 1000), "Walking thing", sf::Style::None);
     window.setFramerateLimit(60);
+    points* two = new points(100, 100, NULL, window);
+    
+    while (1) {
 
-
-
-    //window.clear //uncomment this to make clear screen, to stop screen from smudging 
-    window.display();
-}
+        window.clear({ 0,0,0 }); //uncomment this to make clear screen, to stop screen from smudging 
+        window.display();
+        points one(20, 20, two, window);
+        one.drawat(window);
+    }
+}   
 /*
     if this-> x <next->x
         this->x ++;
