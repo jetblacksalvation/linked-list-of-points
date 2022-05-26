85c2a9b34b7154cb127fdf68f5269c64444fea5c
coolanimeguy666@gmail.com <coolanimeguy666@gmail.com>
2022-05-26 13:45:33 -0600

    not done

--- walking thing.cpp
+++ walking thing.cpp
@@ -3,73 +3,128 @@
 #include <vector>
 #include "sqaure.h"
 square thing(0, 0, 4, 40, true, sf::Color::Blue);
+
 struct points {
-    float x, y;
-    points* next;// looks at the next thing;
-    int tag;
-    
-    points() {
-        x = NULL;
-        y = NULL;
-        next = NULL;
-        tag = NULL;
+public:
+    int x, y;
+    points() : x(NULL), y(NULL) {};
+    points(int x , int y):x(x),y(y) {};
+};
+
+class linked_points {
+private:
+    int* arr_x;
+    int* arr_y;
+    int size; 
+
+public:
+    linked_points() {
+        arr_x = NULL;
+        arr_y = NULL;
+        size = NULL;
     };
-    points(int newx, int newy,points* newthing ,int newtag) {
-        x = newx;
-        y = newy;
-        next = newthing;
-        tag = newtag;
-        
-    }
-    ~points() {//stops data leaks
-
-    };
-    void drawat(sf::RenderWindow& wind) {
-        if (next != NULL) {
-            if (tag == 0 and tag + 1 != NULL) {
-
-                while (this->x != next->x and this->y != next->y) {
-                    thing.x = this->x;
-                    thing.y = this->y;
+    linked_points(std::initializer_list<int> list) {
+        arr_x = (int*)malloc(int(list.size() / 2) * sizeof(int));// this is not working, instead, make to heap allocated integer arrays
+        arr_y = (int*)malloc(int(list.size() / 2) * sizeof(int));
+        size = list.size();
 
 
-                    if (this->x < next->x) {
-                        this->x++;
-                    }
-                    else if (this->x > next->x) {
-                        this->x--;
-                    }
-                    std::cout << thing.x << std::endl;
-                    if (this->y > next->y) {
-                        this->y--;
-                    }
-                    else if (this->y < next->y) {
-                        this->y++;
-                    }
-                    std::cout << thing.y << std::endl;
-                    if (this->x == next->x and this->y == next->y) { std::cout << "exit condition reached\n"; return; }
-                    else {
-                        //the bug is right here, the sqaure stays static 
-                        wind.clear(sf::Color::White);
-                        thing.draw(wind);
-                        wind.display();
-                        thing.update();
 
-                    }
-                }
-            }
 
+        if (list.size() % 2 == 0) {
             
         }
-        if (next == NULL) {
-            std::cout << "is null\n";
+        else {
+            exit;
         }
+        int count = 1;
         
+        int secondcount = 0;
+        for (auto elem : list) {// x comma y
+            
+               
+                if (count %3 == 1) {
+                    arr_x[secondcount] = elem;
+                }
+                else if (count % 3 ==2) {
+                    arr_x[secondcount ] = elem;
+                }
+                else if (count %3 ==0 ) {
+                    secondcount++;
+                }
+                
+                count += 1;
+                
+
+            
+            
+
+           
+        }
+
+
+    }
+    void draw_to_points(sf::RenderWindow& wind) {
+        square* ptr = new square(0,0,4,20,true,sf::Color::Black);
+        ptr->x = arr_x[0];
+        ptr->y = arr_y[0];
+
+
+
+
+
+
+        std::cout << ptr->x<<" is x \n" << std::endl;
+        std::cout << ptr->y<<" is y \n" << std::endl;
+        int tempcount = 0;
+
+        while (tempcount <= size) {
+            std::cout << sizeof(arr_x) << std::endl << std::endl;
+        }
+
+        while (tempcount <= size) {
+            
+            tempcount++;
+            if (ptr->x < arr_x[tempcount]) {
+                ptr->x ++;
+            }
+            else if (ptr->x > arr_x[tempcount]) {
+                ptr->x--;
+            }
+            if (ptr->y < arr_y[tempcount]) {
+                ptr->y++;
+            }
+            else if (ptr->y > arr_y[tempcount]) {
+                ptr-> y--;
+            }
+            if (ptr->y == arr_y[tempcount] and ptr->x == arr_x[tempcount]) {
+                tempcount++;
+            }
+            
+            //drawing logic;
+            wind.clear(sf::Color::White);
+            ptr->draw(wind);
+            ptr->update();
+            
+            
+            wind.display();
+
+        };
+        
+        //end draw
+        
+
+
     };
-    
 };
 
 
+
+
+//wind.clear(sf::Color::White);
+//thing.draw(wind);
+//wind.display();
+//thing.update();
 int main()
 {
 
@@ -77,18 +132,10 @@ int main()
     sf::RenderWindow window(sf::VideoMode(1000, 1000), "Walking thing", sf::Style::None);
     window.setFramerateLimit(60);
 
-    points* three = new points(100, 1000, NULL, 2);
-    points* two = new points(500, 500, three,1);//goal 
-    
+        
     while (1) {
-
-        window.clear({ 255,255,255 }); //uncomment this to make clear screen, to stop screen from smudging 
-        
-        points one(20, 20, two,0);
-        one.x = 20;
-
-        one.drawat(window);
-        
+        linked_points bruh({ 0,0, 50,50 , 100, 20 });
+        bruh.draw_to_points(window);
     }
 }   
 /*
